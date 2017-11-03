3a491ce0aa05decfcd3d10a13d88793fa99c8163
Chris Cardle <cdcardle@gmail.com>
2017-11-03 15:45:47 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |a, b|
     puts "#{a.to_s.capitalize}:"
     b.each do |c, d|
-      puts "  #{c.to_s.gsub(/_/, ' ').capitalize}: #{d.join(", ")}"
+      puts "  #{c.to_s.gsub(/_/, ' ').titleize}: #{d.join(", ")}"
     end
   end
 end
